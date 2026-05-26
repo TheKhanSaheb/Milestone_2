19fb1d260e284573b876ec1d9ebdda2d58698b25
youknowmeright <rakinkhan1001@gmail.com>
2026-05-26 12:05:51 +0600

    initialized app

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
+import app from "./app";
+import config from "./config";
 import { initDB } from "./db";
 
-initDB();
+const main = async () => {
+  await initDB();                 
+  app.listen(config.port, () => {
+    console.log(`DevPulse running on http://localhost:${config.port}`);
+  });
+};
+
+main();
